ec9853837d68fb14111cf16b77bf486eee11de40
dmytr <dmytr@DESKTOP-N8AF4RV>
2021-01-09 19:48:03 +0200

    added new attributes into model

--- src/main/java/com/mycompany/electronicstore/controllers/MobileController.java
+++ src/main/java/com/mycompany/electronicstore/controllers/MobileController.java
@@ -35,7 +35,14 @@ public class MobileController {
     }*/
     
     @GetMapping("/all")
-    public String getAll(Model model){
+    public String getAll(Model model){        
+        model.addAttribute("brands",mobileService.getBrands());
+        model.addAttribute("screenSizes",mobileService.getScreenSizes());
+        model.addAttribute("resolutions",mobileService.getResolutions());
+        model.addAttribute("internalMemories",mobileService.getIntMems());
+        model.addAttribute("operMems", mobileService.getOperMems());
+        model.addAttribute("externalMemories",mobileService.getExtMems());
+        model.addAttribute("cameras",mobileService.getCameras());
         model.addAttribute("mobiles", mobileService.getAllAsHTML());
         return "mobile";
     }
@@ -49,6 +56,13 @@ public class MobileController {
         logger.info(request.getParameter("intMem"));
         logger.info("Sim count:"+Arrays.toString(request.getParameterValues("simCount")));
         logger.info(Arrays.toString(request.getParameterValues("gps")));
+        model.addAttribute("brands",mobileService.getBrands());
+        model.addAttribute("screenSizes",mobileService.getScreenSizes());
+        model.addAttribute("resolutions",mobileService.getResolutions());
+        model.addAttribute("internalMemories",mobileService.getIntMems());
+        model.addAttribute("operMems", mobileService.getOperMems());
+        model.addAttribute("externalMemories",mobileService.getExtMems());
+        model.addAttribute("cameras",mobileService.getCameras());
         model.addAttribute("mobiles", mobileService.getByCriterias(Double.valueOf(request.getParameter("minPrice")),
                 Double.valueOf(request.getParameter("maxPrice")), request.getParameter("screenSize"), 
                 request.getParameter("brand"),/* request.getParameter("resolution"),*/

ff4ed3c4c74687175ddc0fc7b235a494d089076f
dmytr <dmytr@DESKTOP-N8AF4RV>
2021-01-16 21:28:01 +0200

    changed some data, that would be inserted into tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,26 +20,26 @@ INSERT INTO brands(id,name) VALUES(10,"Logitech");
 INSERT INTO brands(id,name) VALUES(11,"Belkin");
 INSERT INTO brands(id,name) VALUES(12,"Meizu");
 
-INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(1,"KWE4501",20000,45,"FULL_HD",true,true,8);
+INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(1,"KD45XH80",20000,45,"FULL_HD",true,true,8);
 INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(2,"UE7600",25000,45,"WQHD",true,true,1);
-INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(3," QE8800",30000,60,"WQHD",true,true,1);
-INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(4," QE5500",25000,45,"WQHD",true,true,1);
-INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(5,"KWE3405",20000,45,"FULL_HD",true,false,8);
-INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(6,"KWE3303",20000,40,"FULL_HD",false,false,8);
+INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(3,"QE7500",30000,60,"WQHD",true,true,1);
+INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(4,"QE5500",25000,45,"WQHD",true,true,1);
+INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(5,"KD55XH90",20000,45,"FULL_HD",true,false,8);
+INSERT INTO TV(id,model,price,screen_size,resolution,smart_tv,3D,brand_id) VALUES(6,"KD43xf7",20000,40,"FULL_HD",false,false,8);
 
 
 INSERT INTO laptops(id,model,price,screen_size,resolution,oper_memory,internal_memory,brand_id)
- VALUES(1,"Macbook",65000,15,"FULL_HD",16,1000,2);
+ VALUES(1,"Macbook Air",65000,15,"FULL_HD",16,1000,2);
 INSERT INTO laptops(id,model,price,screen_size,resolution,oper_memory,internal_memory,brand_id)
- VALUES(2,"Zenbook",20000,15,"FULL_HD",16,1000,3);
+ VALUES(2,"Zenbook UX325JA",20000,15,"FULL_HD",16,1000,3);
 INSERT INTO laptops(id,model,price,screen_size,resolution,oper_memory,internal_memory,brand_id)
- VALUES(3,"Ideapad",15000,15,"FULL_HD",8,500,4);
+ VALUES(3,"Ideapad S145",15000,15,"FULL_HD",8,500,4);
 INSERT INTO laptops(id,model,price,screen_size,resolution,oper_memory,internal_memory,brand_id)
  VALUES(4,"Pavillon",13500,17,"FULL_HD",8,500,5);
 INSERT INTO laptops(id,model,price,screen_size,resolution,oper_memory,internal_memory,brand_id)
- VALUES(5,"Swift",15000,15,"FULL_HD",12,500,6);
+ VALUES(5,"Swift 5",15000,15,"FULL_HD",12,500,6);
 INSERT INTO laptops(id,model,price,screen_size,resolution,oper_memory,internal_memory,brand_id)
- VALUES(6,"ThinkPad",25000,15,"WQHD",16,10000,4);
+ VALUES(6,"ThinkPad E480",25000,15,"WQHD",16,10000,4);
 
 INSERT INTO mobile_devices(id,model,price,screen_size,resolution,oper_memory,internal_memory,
 external_memory,sim_count,camera,gps,brand_id) VALUES(1,"Iphone X",35000,5,"FULL_HD",4,128,0,1,16,true,2);
@@ -60,5 +60,5 @@ external_memory,sim_count,camera,gps,brand_id) VALUES(8,"Redmi Note 9",7000,6,"W
 
 INSERT INTO Accesories(id,model,price,type,short_description,brand_id) VALUES(1,"U20",300,"Earphones","Good earphones",9);
 INSERT INTO Accesories(id,model,price,type,short_description,brand_id) VALUES(2,"M180",600,"Computer mouse","Good computer mouse",10);
-INSERT INTO Accesories(id,model,price,type,short_description,brand_id) VALUES(3,"U20",900,"Keyboard","Good keyboard",10);
-INSERT INTO Accesories(id,model,price,type,short_description,brand_id) VALUES(4,"U20",900,"Bag for laptop","Good bag",11);
+INSERT INTO Accesories(id,model,price,type,short_description,brand_id) VALUES(3,"KL50",900,"Keyboard","Good keyboard",10);
+INSERT INTO Accesories(id,model,price,type,short_description,brand_id) VALUES(4,"B10",900,"Bag for laptop","Good bag",11);

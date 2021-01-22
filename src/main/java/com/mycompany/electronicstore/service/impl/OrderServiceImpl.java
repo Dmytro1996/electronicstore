eba67e9469b6b5c76619fbb82545de9f2a6fed79
dmytr <dmytr@DESKTOP-N8AF4RV>
2021-01-22 16:22:58 +0200

    created OrderServiceImpl

---
+++ src/main/java/com/mycompany/electronicstore/service/impl/OrderServiceImpl.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.electronicstore.service.impl;
+
+import com.mycompany.electronicstore.model.Order;
+import com.mycompany.electronicstore.repository.OrderRepository;
+import com.mycompany.electronicstore.service.OrderService;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author dmytr
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+    
+    private OrderRepository orderRepo;
+    
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+    
+    public Order readById(long id){
+        if(orderRepo.findById(id).isPresent()){
+            return orderRepo.findById(id).get();
+        }
+        throw new EntityNotFoundException("Order with id="+id+" does not exist");
+    }
+
+    public Order create(Order order){
+        return orderRepo.save(order);
+    }
+
+    public Order delete(long id){
+        Order order=readById(id);
+        orderRepo.delete(order);
+        return order;
+    } 
+    
+    public List<Order> getAll(){
+        return orderRepo.findAll().isEmpty()?new ArrayList():orderRepo.findAll();
+    }
+}

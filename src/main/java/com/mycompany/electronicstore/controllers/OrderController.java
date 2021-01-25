8f1fd25a390bb64070bb678e6de38b978dfede7d
dmytr <dmytr@DESKTOP-N8AF4RV>
2021-01-25 19:50:34 +0200

    added remove() method to remove items from basket

--- src/main/java/com/mycompany/electronicstore/controllers/OrderController.java
+++ src/main/java/com/mycompany/electronicstore/controllers/OrderController.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.electronicstore.controllers;
 
+import com.mycompany.electronicstore.details.UserDetailsImpl;
 import com.mycompany.electronicstore.model.Accesorie;
 import com.mycompany.electronicstore.model.Commodity;
 import com.mycompany.electronicstore.model.Laptop;
@@ -12,16 +13,21 @@ import com.mycompany.electronicstore.model.MobileDevice;
 import com.mycompany.electronicstore.model.Order;
 import com.mycompany.electronicstore.model.Television;
 import com.mycompany.electronicstore.service.OrderService;
+import com.mycompany.electronicstore.service.UserService;
+import com.mycompany.electronicstore.service.impl.UserServiceImpl;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -35,6 +41,8 @@ public class OrderController {
     private OrderService orderService;
     Logger logger=LoggerFactory.getLogger(OrderController.class);
     private List<Commodity> basket;
+    @Autowired
+    private UserServiceImpl userService;
     
     @Autowired
     public OrderController(OrderService orderService, List<Commodity> basket){
@@ -72,9 +80,20 @@ public class OrderController {
                 .map(c->(Laptop)c)).collect(Collectors.toList()));
         order.setAcc((basket.stream().filter(c->c instanceof Accesorie)
                 .map(c->(Accesorie)c)).collect(Collectors.toList()));
-        order.setUser(orderService.readById(1).getUser());
+        UserDetailsImpl user=(UserDetailsImpl)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        order.setUser(userService.readById(user.getId()));
         orderService.create(order);
         basket.clear();        
         return "redirect:/index";
+    }
+    
+    @PostMapping("/remove/{index}")
+    public String remove(@RequestHeader("Referer") String referer,@PathVariable("index")int index){
+        basket.remove(index);
+        logger.info(referer);
+        if(referer.contains("filter")){
+            referer=referer.replace("filter", "all");
+        }        
+        return "redirect:"+referer+"?shouldBasketBeOpened=true";
     }
 }

edbab174a1d72b1c88505ef4988ab1791a29107a
dmytr <dmytr@DESKTOP-N8AF4RV>
2021-01-27 23:23:02 +0200

    added bean taht is supposed to make test for controllers access session scoped bean "basket"

--- src/main/java/com/mycompany/electronicstore/Config.java
+++ src/main/java/com/mycompany/electronicstore/Config.java
@@ -7,11 +7,15 @@ package com.mycompany.electronicstore;
 
 import com.mycompany.electronicstore.model.Commodity;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -28,5 +32,15 @@ public class Config {
     public List<Commodity> bakset(){
         return new ArrayList<Commodity>();
     }
+    
+    @Bean
+    public CustomScopeConfigurer scopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        Map<String, Object> workflowScope = new HashMap<String, Object>();
+        workflowScope.put("session", new SimpleThreadScope());
+        configurer.setScopes(workflowScope);
+
+        return configurer;
+  }
     
 }

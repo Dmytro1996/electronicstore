bf6711c39e797da769e84f3bb43a6f0b44111697
Dmytro1996 <dmytrodanylov@ukr.net>
2021-02-03 20:23:51 +0200

    chnged response status and names of some some methods

--- src/main/java/com/mycompany/electronicstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mycompany/electronicstore/exception/GlobalExceptionHandler.java
@@ -10,12 +10,15 @@ import javax.persistence.EntityNotFoundException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  *
@@ -23,30 +26,36 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    Logger logger=LoggerFactory.getLogger(GlobalExceptionHandler.class);
     
     @ExceptionHandler(NullEntityReferenceException.class)
-    @ResponseStatus(value= HttpStatus.NOT_FOUND)
-    public ModelAndView internalServerErrorHandler(HttpServletRequest request, NullEntityReferenceException exception) {
-        return getModelAndView(request, HttpStatus.INTERNAL_SERVER_ERROR, exception);
+    @ResponseStatus(value= HttpStatus.BAD_REQUEST)
+    public ModelAndView nullEntityReferenceExceptionHandler(HttpServletRequest request,
+            NullEntityReferenceException exception) {
+        logger.info("NullEntityReferenceException");
+        return getModelAndView(request, HttpStatus.BAD_REQUEST, exception);
     }
     
     @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView internalServerErrorHandler(HttpServletRequest request, EntityNotFoundException exception) {
-        return getModelAndView(request, HttpStatus.INTERNAL_SERVER_ERROR, exception);
+    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    public ModelAndView entityNotFoundExceptionHandler(HttpServletRequest request,
+            EntityNotFoundException exception) {
+        logger.info("EntityNotFoundException");
+        return getModelAndView(request, HttpStatus.NOT_FOUND, exception);
     }
     
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView internalServerErrorHandler(HttpServletRequest request, Exception exception) {
+        logger.info("Not Found");        
         return getModelAndView(request, HttpStatus.INTERNAL_SERVER_ERROR, exception);
-    }
-    
+    }    
+        
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public void handleAccessDenied(HttpServletRequest request, HttpServletResponse response,
-            AccessDeniedException accessEx) throws IOException,ServletException{
-        response.sendRedirect("/access-denied");
+    public ModelAndView handleAccessDenied(HttpServletRequest request,AccessDeniedException accessEx){
+        logger.info("AccessDenied");
+        return getModelAndView(request, HttpStatus.FORBIDDEN, accessEx);
     }
         
     private ModelAndView getModelAndView(HttpServletRequest request, HttpStatus httpStatus, Exception exception) {        

3b04923ca6bd8e11bd39620d2fdad1fc5cd60f74
Dmytro1996 <dmytrodanylov@ukr.net>
2023-02-12 23:35:01 +0200

    deleted unneeded fields and methods

--- src/main/java/com/mycompany/electronicstore/model/User.java
+++ src/main/java/com/mycompany/electronicstore/model/User.java
@@ -5,16 +5,7 @@
  */
 package com.mycompany.electronicstore.model;
 
-import java.util.List;
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -24,44 +15,27 @@ import org.hibernate.validator.constraints.Length;
  *
  * @author dmytr
  */
-@Entity
-@Table(name="Users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    
     @NotNull
     @Pattern(regexp = "[A-Z][a-z]+",
             message = "Must start with a capital letter followed by one or more lowercase letters")
-    @Column(name="first_name")
     private String firstName;
     @NotNull
     @Pattern(regexp = "[A-Z][a-z]+",
             message = "Must start with a capital letter followed by one or more lowercase letters")
-    @Column(name="last_name")
     private String lastName;
     @NotNull
     @Pattern(regexp = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}", message = "Must be a valid e-mail address")
     @Column
     private String email;
     @NotBlank
-    @Column
     @Pattern(regexp="([0-9]*[A-Za-z]+[0-9]+[\\W]*[A-Za-z]*)+", 
             message="Must be at least 6 characterslong, contain at least 1 digit or 1 number")
     @Length(min=6, message="Must be at least 6 characterslong, contain at least 1 digit or 1 number")
     private String password;
-    @Column
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    private Role role;
-    @OneToMany(mappedBy="user")
-    private List<Order> orders;
 
     public User() {
-    }
-
-    public long getId() {
-        return id;
     }
 
     public String getFirstName() {
@@ -80,18 +54,6 @@ public class User {
         return password;
     }
 
-    public Role getRole() {
-        return role;
-    }
-
-    public List<Order> getOrders() {
-        return orders;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -106,14 +68,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
     }
     
     
